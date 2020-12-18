cd94658cd50bbaa366bfb383796b4b595feb390d
John Mueller <softplus@gmail.com>
2020-12-18 14:45:51 +0100

    Cleanup

--- sc-to-mqtt.py
+++ sc-to-mqtt.py
@@ -18,11 +18,9 @@ from paho.mqtt import client as mqtt_client
 from googleapiclient import sample_tools
 from configparser import ConfigParser
 import json
-import random
 import datetime
 import argparse
 import sys
-import time
 
 CONFIG_FILE = 'sc-to-mqtt.ini'
 
@@ -34,39 +32,32 @@ argparser.add_argument('--add7', help="Add data from 7 days ago", action="store_
 
 def url_to_id(url):
     # Replace cruft from URL to make a MQTT topic ID
-    s = url
-    s = s.replace("http://", "").replace("https://", "").replace("sc-domain:", "")
+    s = url.replace("http://", "").replace("https://", "").replace("sc-domain:", "")
     s = s.replace(":", "_").replace(".", "_").replace("/", "")
     return s
 
-# MQTT debug functions
-def on_connect(client, userdata, flags, rc):
-    print("Connect:   " + str(rc))
 
-def on_message(client, obj, msg):
-    print("Message:   " + msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
-
-def on_log(client, obj, level, string):
-    print("Log:       " + string)
-
-def setup_mqtt(config):
+def connect_mqtt(config):
+    # setup & connect to MQTT client
     client_id = config.get("config", "client_id")
     client = mqtt_client.Client(client_id)
-    client.on_message = on_message
-    client.on_connect = on_connect
-    client.on_log = on_log
+    client.on_log  = lambda client,obj,lev,stri : print("MQTT: ", stri)
     if config.get("config", "mqtt_username"):
-        client.username_pw_set(config.get("config", "mqtt_username"), config.get("config", "mqtt_password"))
+        client.username_pw_set(config.get("config", "mqtt_username"), 
+                               config.get("config", "mqtt_password"))
+    client.connect(config.get("config", "mqtt_broker"), 
+                   int(config.get("config", "mqtt_port")))
     return client
 
 
 def config_sensors(client, sites, config, add_7_day):
+    # Send sensor info for auto-discovery in Home Assistant
     for site in sites:
         siteid = url_to_id(site)
         siteurl = site
         topicid = config.get("config", "mqtt_prefix") + siteid
         conf = {"state_topic": topicid + "/state"}
-        conf.update( { "device": {"name": siteurl, "identifiers": siteurl + "-ID", 
+        conf.update( { "device": {"name": siteurl, "identifiers": siteurl + "#ID", 
             "manufacturer": "Google", "model": "Search Console" } })
         fields = [["Data age", "age", "hrs"], 
                 ["Impressions", "impressions", "x"], 
@@ -82,45 +73,45 @@ def config_sensors(client, sites, config, add_7_day):
             client.publish(topicid + f[1] + "/config", json.dumps(conf))
 
 
-def do_it(service, config, sites, configure_mqtt=True, remove_sensors=False, add_7_day=True):
-    client = setup_mqtt(config)
-    client.connect(config.get("config", "mqtt_broker"), int(config.get("config", "mqtt_port")))
-    if remove_sensors:
-        print("Removing sensors...")
-        for site in sites:
-            siteid = url_to_id(site)
-            for sensor in ["clicks", "impressions", "age", "imps", "clx", "impressions7", "clicks7"]:
-                client.publish(config.get("config", "mqtt_prefix") + siteid + sensor + "/config", "")
-            client.publish(config.get("config", "mqtt_prefix") + siteid + "/config", "")
-        return
+def unconfigure_sensors(config, sites):
+    # remove sensors from Home Asssitant setup by sending empty configs
+    print("Removing sensors...")
+    client = connect_mqtt(config)
+    for site in sites:
+        siteid = url_to_id(site)
+        for sensor in ["clicks", "impressions", "age", "impressions7", "clicks7"]:
+            client.publish(config.get("config", "mqtt_prefix") + siteid + sensor + "/config", "")
+        client.publish(config.get("config", "mqtt_prefix") + siteid + "/config", "")
+        config.set(site, "status", "Unconfigured")
+        config.set(site, "status_date", datetime.datetime.utcnow().isoformat())
+
+
+def do_it(service, config, sites, configure_mqtt=True, add_7_day=True):
+    # Connect to MQTT, Get SC data, Send to MQTT
+    client = connect_mqtt(config)
     if configure_mqtt: config_sensors(client, sites, config, add_7_day)
 
     # request data from last 7 days; use last entry
     start_date = (datetime.datetime.utcnow() + datetime.timedelta(days=-7)).strftime("%Y-%m-%d")
     end_date = (datetime.datetime.utcnow() + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
-
-    # get last datapoint dates
-    request = {
-      'startDate': start_date, 'endDate': end_date,
-      'dimensions': ['date'], 'dataState': 'all'
-    }
+    request = { 'startDate': start_date, 'endDate': end_date, 'dimensions': ['date'], 'dataState': 'all' }
 
     for site in sites:
-        print("Processing ", site)
         response = service.searchanalytics().query(siteUrl=site, body=request).execute()
 
         if "rows" in response:
+            # Get the freshest data, nom nom
             row = response["rows"][-1]
-            print("Data row: ", row)
+
+            if not config.has_section(site): config.add_section(site)
+
+            # calculate age in hours (hacky, since we just have a date)
             fresh_date = row["keys"][0]
-
-            section_name = site
-            if not config.has_section(section_name): config.add_section(section_name)
-
             data_age = (datetime.datetime.utcnow() - 
                 datetime.datetime.strptime(fresh_date, '%Y-%m-%d'))
             data_age_hrs = round(data_age.days*24 + data_age.seconds/(60*60), 1)
 
+            # Create & send MQTT message
             data = {"impressions": row["impressions"], "clicks": row["clicks"], "age": data_age_hrs}
             if add_7_day:
                 data["impressions7"] = response["rows"][0]["impressions"]
@@ -128,12 +119,15 @@ def do_it(service, config, sites, configure_mqtt=True, remove_sensors=False, add
             topicid = config.get("config", "mqtt_prefix") + url_to_id(site)
             client.publish(topicid + '/state', json.dumps(data))
 
-            config.set(section_name, "last_row", json.dumps(row))
-            config.set(section_name, "last_run", datetime.datetime.utcnow().isoformat())
+            # Log to config file
+            config.set(site, "last_row", json.dumps(row))
+            config.set(site, "status", "Sent")
+            config.set(site, "status_date", datetime.datetime.utcnow().isoformat())
+
 
 def main(argv):
-    # connect to SC
-    print(datetime.datetime.utcnow().isoformat(), " - ", __file__)
+    # connect to SC, etc
+    print(datetime.datetime.utcnow().isoformat(), " - ", __file__, " - started")
 
     service, flags = sample_tools.init(
         argv, 'searchconsole', 'v1', __doc__, __file__, parents=[argparser],
@@ -150,23 +144,28 @@ def main(argv):
     if not state.has_option("config", "mqtt_password"): state.set("config", "mqtt_password", "")
     if not state.has_option("config", "client_id"): state.set("config", "client_id", "pythonForSeo")
     if not state.has_option("config", "mqtt_prefix"): state.set("config", "mqtt_prefix", "homeassistant/sensor/sc_")
-    with open(flags.config, 'w') as configfile: state.write(configfile)
+    with open(flags.config, "w") as configfile: state.write(configfile)
 
     if not state.has_option("config", "sites"): 
-        # Filter for verified websites
+        # Get some verified site URLs
         site_list = service.sites().list().execute()
-        verified_sites_urls = [s['siteUrl'] for s in site_list['siteEntry']
-                            if s['permissionLevel'] != 'siteUnverifiedUser' and s['siteUrl'][:4] == 'http']
+        verified_sites_urls = [s['siteUrl'] for s in site_list['siteEntry'] if s['permissionLevel'] != 'siteUnverifiedUser']
         sites = verified_sites_urls[:2] # pick first 2 verified sites to try out
         print("Using these verified sites: ", sites)
         state.set("config", "sites", ", ".join(sites))
-        with open(flags.config, 'w') as configfile: state.write(configfile)
+        with open(flags.config, "w") as configfile: state.write(configfile)
 
     sites = state.get("config", "sites").split(",")
     sites = [x.strip() for x in sites]
-    do_it(service, state, sites, configure_mqtt=(not flags.noconfig), remove_sensors=flags.remove, add_7_day=flags.add7)
 
-    with open(flags.config, 'w') as configfile: state.write(configfile)
+    if flags.remove:
+        unconfigure_sensors(state, sites)
+    else:
+        do_it(service, state, sites, configure_mqtt=(not flags.noconfig), add_7_day=flags.add7)
 
-main(sys.argv)
+    with open(flags.config, "w") as configfile: state.write(configfile)
+    print(datetime.datetime.utcnow().isoformat(), " - ", __file__, " - done")
 
+
+if __name__ == '__main__':
+    main(sys.argv)
